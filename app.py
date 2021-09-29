a91b45c8f01ead13988fe47f6ce19d3ca528f557
Lukasz Buko <65955437+look1976@users.noreply.github.com>
2021-09-29 13:31:39 +0200

    change output message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello Steamtalks !!! How are you doing ???"
+    return "Hello Steamtalks !!! How are you doing ??? New version 1.1"
 
 @app.route('/how are you')
 def hello():
